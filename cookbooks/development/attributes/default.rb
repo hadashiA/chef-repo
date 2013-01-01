069422607c4b4c2ca6a4b2d51b8e4e8793d80aff
hadashi <dev@hadashikick.jp>
2013-01-02 02:02:57 +0900

    update Cheffile.lock

--- cookbooks/development/attributes/default.rb
+++ cookbooks/development/attributes/default.rb
@@ -1,6 +1,6 @@
 default['development']['username'] = 'hadashi'
 
-default['development']['rbenv']['rubies'] = ['1.9.3-p327', '2.0.0-dev']
+default['development']['rbenv']['rubies'] = ['1.9.3-p327']
 default['development']['rbenv']['gems'] = [{:name => 'bundler'},
                                            {:name => 'pry'},
                                            {:name => 'pry-doc'},
@@ -8,4 +8,4 @@ default['development']['rbenv']['gems'] = [{:name => 'bundler'},
                                            {:name => 'rak'},
                                            {:name => 'devel-which'},
                                           ]
-default['development']['rbenv']['global'] = '2.0.0-dev'
+default['development']['rbenv']['global'] = '1.9.3-p327'
