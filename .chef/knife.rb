3e1870df5016f679837b16ac3b8f94deba0a68dc
f-kubotar <dev@hadashikick.jp>
2013-05-12 13:59:37 +0900

    knife.rb fullpath

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,4 +1,6 @@
-cookbook_path ["cookbooks", "site-cookbooks"]
-role_path     "roles"
-data_bag_path "data_bags"
+repo = File.expand_path('../..', __FILE__)
+
+cookbook_path ["#{repo}/cookbooks", "#{repo}/site-cookbooks"]
+role_path     "#{repo}/roles"
+data_bag_path "#{repo}/data_bags"
 #encrypted_data_bag_secret "data_bag_key"
